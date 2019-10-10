ebdd5e727cf97b3f54d9833cc1d49254773dbdf8
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2019-10-10 10:36:47 +0100

    Update GA script

--- analytics.js
+++ analytics.js
@@ -1,80 +1,173 @@
-//In Google Analytics you'll need to set up custom dimensions as follows
+// Updated Aug 28, 2019
+// In Google Analytics you'll need to set up custom dimensions as follows
 // Custom Dimension 1 = Canvas User ID --- Scope = User
 // Custom Dimension 2 = Archived --- Scope = User
 // Custom Dimension 3 = Canvas User Role --- Scope = User
 // Custom Dimension 4 = Canvas Course ID --- Scope = Hit
 // Custom Dimension 5 = Canvas Course Name --- Scope = Hit
 // Custom Dimension 6 = Canvas Sub-Account ID --- Scope = Hit
+// Custom Dimension 7 = Canvas Term ID --- = Scope = Hit
+// Custom Dimension 8 = Canvas Course Role --- Scope = Hit
 
 (function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
     m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'custom_ga');
 
-$(document).ready(function () {
+(function () {
+  function removeStorage(key) {
+    try {
+      localStorage.removeItem(key);
+      localStorage.removeItem(key + '_expiresIn');
+    } catch (e) {
+      console.log('removeStorage: Error removing key [' + key + '] from localStorage: ' + JSON.stringify(e));
+      return false;
+    }
+    return true;
+  }
 
-    // START - More Google Analytics Tracking Code
-    var sUserId
-    var sUserRole
-    var sTemp // Course ID from URL
-    var _course
-    var sCourseName = null
-    var parent_account //Give you the subaccount_id that the course is in
-
-    ga('create', 'UA-129584901-1', 'auto');
-
-    //Get User Information
-    sUserId = ENV["current_user_id"]
-    ga('set', 'dimension1', sUserId);
-
-
-    //Get User Role
-    if ($.inArray('admin', ENV['current_user_roles']) == -1 && $.inArray('teacher', ENV['current_user_roles']) == -1 && $.inArray('student', ENV['current_user_roles']) > -1) {
-        sUserRole = "student"
-    } else if ($.inArray('admin', ENV['current_user_roles']) == -1 && $.inArray('teacher', ENV['current_user_roles']) > -1) {
-        sUserRole = "teacher"
-    } else if ($.inArray('admin', ENV['current_user_roles']) > -1) {
-        sUserRole = "admin"
-    } else {
-        sUserRole = "other"
+  function getStorage(key) {
+    var now = Date.now(); //epoch time, lets deal only with integer
+    // set expiration for storage
+    var expiresIn = localStorage.getItem(key + '_expiresIn');
+    if (expiresIn === undefined || expiresIn === null) {
+      expiresIn = 0;
     }
 
-    ga('set', 'dimension3', sUserRole);
-    // This currently loses the last octet of the address
-    ga('set', 'anonymizeIp', true);
+    if (expiresIn < now) { // Expired
+      removeStorage(key);
+      return null;
+    } else {
+      try {
+        var value = localStorage.getItem(key);
+        return value;
+      } catch (e) {
+        console.log('getStorage: Error reading key [' + key + '] from localStorage: ' + JSON.stringify(e));
+        return null;
+      }
+    }
+  }
 
-    //If the user is in a course
+  function setStorage(key, value, expires) {
+    if (expires === undefined || expires === null) {
+      expires = (24 * 60 * 60); // default: seconds for 6 hours (6*60*60)
+    } else {
+      expires = Math.abs(expires); //make sure it's positive
+    }
+
+    var now = Date.now(); //millisecs since epoch time, lets deal only with integer
+    var schedule = now + expires * 1000;
     try {
-        sTemp = window.location.pathname.match(/\/courses\/(\d+)/);
-        if (sTemp[1]) {
+      localStorage.setItem(key, value);
+      localStorage.setItem(key + '_expiresIn', schedule);
+    } catch (e) {
+      console.log('setStorage: Error setting key [' + key + '] in localStorage: ' + JSON.stringify(e));
+      return false;
+    }
+    return true;
+  }
 
-            //Get Course information - Course Name and parent sub-account id
-            var d1 = $.get('/api/v1/courses/' + sTemp[1], function (_course) {
-                parent_account = _course.account_id
-                parent_account = parent_account.toString();
-                sCourseName = _course.name
-            });
+  async function coursesRequest(courseId) {
+    //
+    let response = await fetch('/api/v1/users/self/courses?per_page=100');
+    let data = await response.text();
+    data = data.substr(9);
+    data = JSON.parse(data)
+    var stringData = JSON.stringify(data)
+    setStorage('ga_enrollments', stringData, null)
+    var course = parseCourses(courseId, stringData)
+    return course
+  };
 
-
-            $.when(d1).done(function (_account) {
-                // ...do stuff...
-                ga('set', 'dimension4', sTemp[1]);
-                ga('set', 'dimension5', sCourseName);
-                ga('set', 'dimension6', parent_account);
-                ga('send', 'pageview');
-            });
-        } else {
-            ga('send', 'pageview');
+  function parseCourses(courseId, courseData) {
+    if (courseData != undefined) {
+      let data = JSON.parse(courseData);
+      //console.log(data)
+      for (var i = 0; i < data.length; i++) {
+        // console.log(data[i]['id'] + " " + courseId)
+        if (data[i]['id'] == courseId) {
+          return data[i]
         }
-    } catch (err) {}
+      }
+    }
+    return null
+  }
 
-    // END - Google Analytics Tracking Code
+  function gaCourseDimensions(course) {
+    custom_ga('set', 'dimension4', course['id']);
+    custom_ga('set', 'dimension5', course['name']);
+    custom_ga('set', 'dimension6', course['account_id']);
+    custom_ga('set', 'dimension7', course['enrollment_term_id']);
+    custom_ga('set', 'dimension8', course['enrollments'][0]['type']);
+    custom_ga('send', 'pageview');
+    return
+  }
 
-});
+  function googleAnalyticsCode(trackingID) {
+    var userId, userRoles, attempts, courseId;
+    custom_ga('create', trackingID, 'auto');
+    userId = ENV["current_user_id"];
+    userRoles = ENV['current_user_roles'];
+    custom_ga('set', 'userId', userId);
+    custom_ga('set', 'dimension1', userId);
+    custom_ga('set', 'dimension3', userRoles);
+    courseId = window.location.pathname.match(/\/courses\/(\d+)/);
+    if (courseId) {
+      courseId = courseId[1];
+      attempts = 0;
+      try {
+        let courses = getStorage('ga_enrollments')
+        if (courses != null) {
+          var course = parseCourses(courseId, courses);
+          if (course === null) {
+            // console.log("course_id not found in cache, retrieving...")
+            coursesRequest(courseId).then(course => {
+              if (course === null) {
+                // console.log("course data not found")
+                custom_ga('set', 'dimension4', courseId);
+                custom_ga('send', 'pageview');
+              } else {
+                gaCourseDimensions(course)
+              }
+            });
+          } else {
+            // console.log("course found in cache")
+            gaCourseDimensions(course)
+          }
+        } else {
+          // console.log("cache not found, retrieving cache data")
+          coursesRequest(courseId).then(course => {
+            if (course === null) {
+              // console.log("course data not found")
+              custom_ga('set', 'dimension4', courseId);
+              custom_ga('send', 'pageview');
+            } else {
+              gaCourseDimensions(course)
+            }
+          });
+        }
+      } catch (err) {
+        attempts += 1;
+        if (attempts > 5) {
+          custom_ga('set', 'dimension4', courseId);
+          custom_ga('send', 'pageview');
+          return;
+        }
+        ;
+      }
+      ;
+    } else {
+      custom_ga('send', 'pageview');
+    }
+    ;
+  };
+
+  googleAnalyticsCode("UA-129584901-1")
+})();
